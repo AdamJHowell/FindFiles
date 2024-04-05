55b9bc24724c410c7d36065caea990eacde46635
Adam Howell <AdamJHowell@users.noreply.github.com>
2024-04-04 19:57:12 -0600

    Improvements

--- FindFiles.py
+++ FindFiles.py
@@ -8,6 +8,9 @@ from typing import List
 def find_audio_files( directory: str, extensions: List[str] ) -> List[str]:
   """
   List all files in the specified directory and its subdirectories that have the given extensions.
+  @param directory: The directory to search for audio files.
+  @param extensions: The extensions to search for.
+  @return: A list of audio files.
   """
   audio_files = []
   for root, dirs, files in os.walk( directory ):
@@ -18,15 +21,26 @@ def find_audio_files( directory: str, extensions: List[str] ) -> List[str]:
 
 
 if __name__ == "__main__":
-  output_filename = "music-" + datetime.now().isoformat() + ".txt"
-  output_filename = output_filename.replace( ":", "." )
+  program_name = "FindFiles"
+  current_time = datetime.now().strftime( "%Y-%m-%dT%H.%M.%S" )
+  print( f"{program_name} starting at {current_time}" )
+  output_filename = "music-" + current_time + ".txt"
+
   # Use the current working directory as the starting point, or replace it with the desired directory path.
   current_directory = os.getcwd()
+
   # A list of all audio file extensions to look for.
   extension_list = ["flac", "m4a", "mp3", "wav", "aac", "ogg"]
   results_string = ""
+
+  # Call find_audio_files and parse the returned list.
   for audio_file in find_audio_files( current_directory, extension_list ):
-    print( audio_file )
     results_string += audio_file + "\n"
-  with open( output_filename, 'w', encoding = 'utf-8', newline = '\n' ) as key_file:
-    write_bytes = key_file.write( results_string )
+
+  if len( results_string ) > 0:
+    # Save the results to a file.
+    with open( output_filename, 'w', encoding = 'utf-8', newline = '\n' ) as key_file:
+      write_bytes = key_file.write( results_string )
+      print( f"Wrote {write_bytes} bytes to {output_filename}" )
+  else:
+    print( f"No audio files found at {current_directory}" )
